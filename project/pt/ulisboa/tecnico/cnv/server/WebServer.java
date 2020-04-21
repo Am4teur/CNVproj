3c305a5031452f86294832f61602448c35b91124
Bernardo Marques <bernardocmarques@tecnico.ulisboa.pt>
2020-04-21 23:11:40 +0100

    fix merge

--- project/pt/ulisboa/tecnico/cnv/server/WebServer.java
+++ project/pt/ulisboa/tecnico/cnv/server/WebServer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package pt.ulisboa.tecnico.cnv.server;
 
 import com.sun.net.httpserver.Headers;
@@ -21,158 +20,6 @@ import java.util.concurrent.Executors;
 
 import BIT.*;
 
-
-public class WebServer {
-
-	public static void main(final String[] args) throws Exception {
-
-		//final HttpServer server = HttpServer.create(new InetSocketAddress("127.0.0.1", 8000), 0);
-
-		final HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8000), 0);
-		
-
-
-		server.createContext("/sudoku", new MyHandler());
-
-		// be aware! infinite pool of threads!
-		server.setExecutor(Executors.newCachedThreadPool());
-		server.start();
-
-		System.out.println("Server started at: " + server.getAddress().toString());
-	}
-
-	public static String parseRequestBody(InputStream is) throws IOException {
-        InputStreamReader isr =  new InputStreamReader(is,"utf-8");
-        BufferedReader br = new BufferedReader(isr);
-
-        // From now on, the right way of moving from bytes to utf-8 characters:
-
-        int b;
-        StringBuilder buf = new StringBuilder(512);
-        while ((b = br.read()) != -1) {
-            buf.append((char) b);
-
-        }
-
-        br.close();
-        isr.close();
-
-        return buf.toString();
-    }
-	static class MyHandler implements HttpHandler {
-		@Override
-		public void handle(final HttpExchange t) throws IOException {
-
-			// Get the query.
-			final String query = t.getRequestURI().getQuery();
-			System.out.println("> Query:\t" + query);
-
-			// Break it down into String[].
-			final String[] params = query.split("&");
-
-			// Store as if it was a direct call to SolverMain.
-			final ArrayList<String> newArgs = new ArrayList<>();
-			for (final String p : params) {
-				final String[] splitParam = p.split("=");
-				newArgs.add("-" + splitParam[0]);
-				newArgs.add(splitParam[1]);
-			}
-			newArgs.add("-b");
-			newArgs.add(parseRequestBody(t.getRequestBody()));
-
-			newArgs.add("-d");
-
-			// Store from ArrayList into regular String[].
-			final String[] args = new String[newArgs.size()];
-			int i = 0;
-			for(String arg: newArgs) {
-				args[i] = arg;
-				i++;
-			}
-			// Get user-provided flags.
-			final SolverArgumentParser ap = new SolverArgumentParser(args);
-
-			// Create solver instance from factory.
-			final Solver s = SolverFactory.getInstance().makeSolver(ap);
-
-			//Solve sudoku puzzle
-			JSONArray solution = s.solveSudoku();
-
-
-			BufferedWriter out = null;
-
-			try {
-				File file = new File("/home/am4teur/cnv/tempBD.txt");
-				FileWriter fstream = new FileWriter(file, true); //true tells to append data.
-				out = new BufferedWriter(fstream);
-				System.out.println("PRINT TO FILE");
-				out.write("\n");
-				out.write("> Query:\t" + query + "\n");
-				out.write("> Counter:\t" + ICount.getICount());
-				System.out.println("PRINT TO FILE DONE");
-			}
-			
-			catch (IOException e) {
-				System.err.println("Error: " + e.getMessage());
-			}
-			
-			finally {
-				if(out != null) {
-					out.close();
-				}
-			}
-
-
-			// Send response to browser.
-			final Headers hdrs = t.getResponseHeaders();
-
-            //t.sendResponseHeaders(200, responseFile.length());
-
-
-			///hdrs.add("Content-Type", "image/png");
-            hdrs.add("Content-Type", "application/json");
-
-			hdrs.add("Access-Control-Allow-Origin", "*");
-
-            hdrs.add("Access-Control-Allow-Credentials", "true");
-			hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
-			hdrs.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-            t.sendResponseHeaders(200, solution.toString().length());
-
-
-            final OutputStream os = t.getResponseBody();
-            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-            osw.write(solution.toString());
-            osw.flush();
-            osw.close();
-
-			os.close();
-
-			System.out.println("> Sent response to " + t.getRemoteAddress().toString());
-		}
-	}
-}
-=======
-package pt.ulisboa.tecnico.cnv.server;
-
-import com.sun.net.httpserver.Headers;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import org.json.JSONArray;
-import pt.ulisboa.tecnico.cnv.solver.Solver;
-import pt.ulisboa.tecnico.cnv.solver.SolverArgumentParser;
-import pt.ulisboa.tecnico.cnv.solver.SolverFactory;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.concurrent.Executors;
 
 public class WebServer {
 
@@ -251,6 +98,30 @@ public class WebServer {
 			JSONArray solution = s.solveSudoku();
 
 
+			BufferedWriter out = null;
+
+			try {
+				File file = new File("tempBD.txt");
+				FileWriter fstream = new FileWriter(file, true); //true tells to append data.
+				out = new BufferedWriter(fstream);
+				System.out.println("PRINT TO FILE");
+				out.write("\n");
+				out.write("> Query:\t" + query + "\n");
+				out.write("> Counter:\t" + ICount.getICount());
+				System.out.println("PRINT TO FILE DONE");
+			}
+			
+			catch (IOException e) {
+				System.err.println("Error: " + e.getMessage());
+			}
+			
+			finally {
+				if(out != null) {
+					out.close();
+				}
+			}
+
+
 			// Send response to browser.
 			final Headers hdrs = t.getResponseHeaders();
 
@@ -280,7 +151,6 @@ public class WebServer {
 			System.out.println("> Sent response to " + t.getRemoteAddress().toString());
 		}
 	}
-
 	static class Hello implements HttpHandler {
 		@Override
 		public void handle(HttpExchange t) throws IOException {
@@ -292,4 +162,3 @@ public class WebServer {
 		}
 	}
 }
->>>>>>> c6c3dc319ad36a7ff2e2cfa31ea0e58880b9d33f

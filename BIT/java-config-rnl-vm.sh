48b02aaf62a4d4cb0dd026b33e72d7b234311719
Bernardo Marques <bernardocmarques@tecnico.ulisboa.pt>
2020-05-22 19:37:57 +0100

    classpath changed

--- BIT/java-config-rnl-vm.sh
+++ BIT/java-config-rnl-vm.sh
@@ -5,4 +5,4 @@ export JRE_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
 export PATH=/usr/lib/jvm/java-7-openjdk-amd64/bin/:$PATH
 export SDK_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
-export CLASSPATH=%CLASSPATH%:~/cnv/instrumented:~/cnv/project:~/cnv/BIT:~/cnv/BIT/samples:~/cnv/aws-java-sdk-1.11.788/lib/aws-java-sdk-1.11.788.jar:~/cnv/aws-java-sdk-1.11.785/third-party/lib/*:.
+export CLASSPATH=%CLASSPATH%:~/cnv/instrumented:~/cnv/project:~/cnv/BIT:~/cnv/BIT/samples:~/aws/lib/aws-java-sdk-1.11.788.jar:~/aws/third-party/lib/*:.
